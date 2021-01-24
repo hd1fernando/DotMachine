f67f68280726b7d6921c290a136d2d611a2df226
h1fernando <hd1.fernandosouza@gmail.com>
2021-01-24 20:27:27 -0300

    feat: add simple operations

---
+++ src/DotMachine/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static System.Console;
+
+namespace VirtualMachine
+{
+    class Program
+    {
+
+        static string[] program;
+
+        static void LoadProgram()
+        {
+            StringBuilder sb = new();
+            sb.Append("PSH 6 ");
+            sb.Append("PSH 6 ");
+            sb.Append("ADD ");
+            sb.Append("POP ");
+            sb.Append("HLT ");
+            program = sb.ToString().Split(' ');
+        }
+
+        static int ip = 0; // Instruction pointer
+        static int sp = -1; // stack pointer
+        static int[] stack = new int[256];
+
+        static bool running = true;
+
+        static void Main(string[] args)
+        {
+            LoadProgram();
+            while (running)
+            {
+                Eval(Fetch());
+                ip++;
+            }
+
+            ReadLine();
+        }
+
+        static void Eval(string instruction)
+        {
+            InstructionsSet[instruction].Invoke();
+        }
+
+        static Dictionary<string, Action> InstructionsSet = new()
+        {
+            { "HLT", () => { running = false; } },// Stop the program
+            { "PSH", PushRule },
+            { "POP", PopRule },
+            { "ADD", AddRule }
+        };
+
+
+        static void PushRule()
+        {
+            IncrementSP();
+            stack[sp] = int.Parse(program[++ip]);
+        }
+
+        static int PopFromStack() => stack[sp--];
+        static int IncrementSP() => sp++;
+        static void PopRule()
+        {
+            int valPoped = PopFromStack();
+
+            WriteLine($"POPED: {valPoped}");
+        }
+
+        static void AddRule()
+        {
+            int a = PopFromStack();
+            int b = PopFromStack();
+            int result = a + b;
+            IncrementSP();
+            stack[sp] = result;
+        }
+
+        static string Fetch()
+            => program[ip];
+    }
+
+    enum Registers
+    {
+        A, B, C, D, E, F, PC, SP, NUM_OF_REGISTERS
+    }
+
+    enum InstructionSet : int
+    {
+        PSH, // Push a value to Stack
+        ADD, // Pops two values on top of the stack, adds them pushes to stack
+        POP, // Pops a value from stack
+        SET, // Sets a value to a register
+        HLT // Stop the program
+    };
+}
