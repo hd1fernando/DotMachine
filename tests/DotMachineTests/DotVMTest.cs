841a1face82cf269e40aec35124198a7888e316c
hd1fernando <hd1.fernandosouza@hotmail.com>
2021-01-26 18:15:58 -0300

    tests: tests to MUL and more tests for ADD instruction

--- tests/DotMachineTests/DotVMTest.cs
+++ tests/DotMachineTests/DotVMTest.cs
@@ -6,12 +6,16 @@ namespace DotMachineTests
 {
     public class DotVMTest
     {
-        [Fact]
-        public void Add_ShouldAddTwoValues_TwoPositiveNumbers()
+        [Theory(DisplayName = "Add two numbers")]
+        [InlineData("5", "6", 11)]
+        [InlineData("5", "-6", -1)]
+        [InlineData("-5", "6", 1)]
+        [InlineData("-5", "-6", -11)]
+        public void Add_ShouldAddTwoValues(string valuea, string valueb, int result)
         {
-            string program = @"
-            PUSH 5
-            PUSH 6
+            string program = $@"
+            PUSH {valuea}
+            PUSH {valueb}
             ADD
             POP
             HLT";
@@ -22,7 +26,30 @@ namespace DotMachineTests
 
             var eax = vm.EAX.Get();
 
-            eax.Should().Be(11);
+            eax.Should().Be(result);
+        }
+
+        [Theory(DisplayName = "Mul two numbers")]
+        [InlineData("5", "6", 30)]
+        [InlineData("5", "-6", -30)]
+        [InlineData("-5", "6", -30)]
+        [InlineData("-5", "-6", 30)]
+        public void ShouldMulTwoValues(string valuea, string valueb, int result)
+        {
+            string program = $@"
+            PUSH {valuea}
+            PUSH {valueb}
+            MUL
+            POP
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var eax = vm.EAX.Get();
+
+            eax.Should().Be(result);
         }
     }
 }
