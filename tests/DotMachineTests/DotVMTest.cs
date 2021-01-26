f5a24db62d6f29182192edff5e1d5f657a45aae4
h1fernando <hd1.fernandosouza@gmail.com>
2021-01-25 22:18:29 -0300

    tests: create tests for VM

---
+++ tests/DotMachineTests/DotVMTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using VirtualMachine;
+using Xunit;
+
+namespace DotMachineTests
+{
+    public class DotVMTest
+    {
+        [Fact]
+        public void Add_ShouldAddTwoValues_AGraterThanB()
+        {
+            string program = @"
+            PUSH 5
+            PUSH 6
+            ADD
+            POP
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var eax = vm.EAX.Get();
+
+            eax.Should().Be(11);
+        }
+    }
+}
