881e23410ec884ca213cab54c90148a17a420505
hd1fernando <hd1.fernandosouza@hotmail.com>
2021-01-26 21:15:54 -0300

    test: tests for instructions MUL, SUB, JMP, CPM, XOR

--- tests/DotMachineTests/DotVMTest.cs
+++ tests/DotMachineTests/DotVMTest.cs
@@ -29,6 +29,28 @@ namespace DotMachineTests
             eax.Should().Be(result);
         }
 
+        [Theory(DisplayName = "Sub two numbers")]
+        [InlineData("5", "6", 1)]
+        [InlineData("5", "-6", -11)]
+        [InlineData("-5", "6", 11)]
+        [InlineData("-5", "-6", -1)]
+        public void Add_ShouldSubTwoValues(string valuea, string valueb, int result)
+        {
+            string program = $@"
+            PUSH {valuea}
+            PUSH {valueb}
+            SUB
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var eax = vm.EAX.Get();
+
+            eax.Should().Be(result);
+        }
+
         [Theory(DisplayName = "Mul two numbers")]
         [InlineData("5", "6", 30)]
         [InlineData("5", "-6", -30)]
@@ -50,6 +72,65 @@ namespace DotMachineTests
             var eax = vm.EAX.Get();
 
             eax.Should().Be(result);
+        }
+
+        [Theory(DisplayName = "Compare two numbers")]
+        [InlineData("5", "6", 0)]
+        [InlineData("5", "5", 1)]
+        public void ShoulCompareTwoValues(string valuea, string valueb, int result)
+        {
+            string program = $@"
+            PUSH {valuea}
+            PUSH {valueb}
+            CMP
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var zf = vm.ZF.Get();
+
+            zf.Should().Be(result);
+        }
+
+        [Fact(DisplayName = "Jump for position at code")]
+        public void ShoudJump()
+        {
+            string program = $@"
+            PUSH 4
+            JMP
+            CMP
+            PUSH 5
+            PUSH 5
+            ADD
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var eax = vm.EAX.Get();
+
+            eax.Should().Be(10);
+        }
+
+        [Fact(DisplayName = "XOR two numbers")]
+        public void ShoudXor()
+        {
+            string program = $@"
+            PUSH 5
+            PUSH 5
+            XOR
+            HLT";
+            DotVM vm = new(program);
+            vm.Load();
+
+            vm.Exec();
+
+            var eax = vm.EAX.Get();
+
+            eax.Should().Be(0);
         }
     }
 }
